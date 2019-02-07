13937262454ebf95b6193cc3c3b64f4f53386fdb
gabrielstik <gabriel.stik@gmail.com>
2019-02-08 00:07:41 +0100

    made home

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,32 @@
 <template>
 <div id="header" class="header" ref="header">
-
+  <nav>
+    <ul>
+      <li v-for="link in data.nav" :key="link.id">
+        <a v-if="link.link.match(/(\/\/|mailto:).*/g)" :href="link.link" :title="link.label" class="link">{{link.label}}</a>
+        <nuxt-link v-else :to="link.link" class="link">{{link.label}}</nuxt-link>
+      </li>
+    </ul>
+  </nav>
 </div>
 </template>
 
 <script>
+import api from '~/api/data.json'
+
 export default {
   name: 'Header',
   props: [],
-  mounted: function() {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  created() {
+    this.data = api.header
+  },
+  mounted() {
+    console.log(this.data)
   },
   methods: {
   }
@@ -16,5 +34,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#header {
+  position: fixed;
+  width: 100vh;
+  height: 100px;
+  border-bottom: solid 1px var(--lightGrey);
+  transform-origin: top left;
+  transform: translateY(100vh) rotate(-90deg);
+}
 
+nav {
+  display: flex;
+  align-items: center;
+}
+
+nav ul {
+  width: 60vh;
+  margin: 30px auto;
+  display: flex;
+  justify-content: space-between;
+
+  .link {
+    transform: rotate(-90deg);
+    font-family: 'Graphik', sans-serif;
+    font-size: 16px;
+    color: var(--main);
+    letter-spacing: 0;
+    text-align: center;
+    line-height: 33px;
+  }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
 <div id="home" class="home" ref="main">
-
+  <h1 class="title">{{data.title}}</h1>
+  <p class="text">{{data.text}}</p>
 </div>
 </template>
 
 <script>
+import api from '~/api/data.json'
 import { TimelineMax } from 'gsap'
 
 export default {
@@ -18,14 +20,14 @@ export default {
     }
   },
   created: function() {
-    this.data = wp.pages.home
+    this.data = api.home
   },
   mounted: function() {
     if (this.$store.state.fromRoute == null) this.mountAnimation(1.5)
     else this.mountAnimation(0)
     this.$store.commit('updateRoute', this.$route.path)
 
-
+    console.log(this.api)
   },
   beforeRouteLeave: function(to, from, next) {
     next()
@@ -38,4 +40,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#home {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  background-color: var(--background);
+}
+
+.title {
+  font-family: 'PTSerif', serif;
+  font-size: 100px;
+  color: var(--main);
+  letter-spacing: 0;
+  text-align: center;
+}
+
+.text {
+  width: 440px;
+  margin: 44px auto;
+  font-family: 'Maison Neue', sans-serif;
+  font-size: 18px;
+  color: var(--grey);
+  letter-spacing: 0;
+  text-align: center;
+  line-height: 33px;
+}
 </style>
